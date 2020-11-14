36be6caff8b1651668a330e725fea152f031face
Joel Nash <joelnash@xuuki.com>
2020-11-14 22:36:20 +0000

    asp.net core framework added

---
+++ aspnetcoreapp/Models/Creative.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesCreative.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Vyvyan Mar { get; set; }
+
+    }
+}
